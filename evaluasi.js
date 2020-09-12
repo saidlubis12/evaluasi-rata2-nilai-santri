440455a4fa0fe0e28bd940399d688b5bb9a98044
ninjaa <alkhudrisaid83@gmail.com>
2020-09-12 07:10:01 +0700

    first commit

--- evaluasi.js
+++ evaluasi.js
@@ -1,23 +1,3 @@
-// var nilaisantri = [ 5,9,6,7,9,8,10,7,8];
-// var total = 0;
-// var isinilai = nilaisantri.length;
-
-
-// for (i=0; i < isinilai; i++){
-//     console.log(i);
-//     total += nilaisantri[i]
-
-// }
-// document.write(total / isinilai);
-
-
-
-
-
-
-
-
-
 
 var nilaisantri = [50, 90, 65, 67, 89, 80, 50, 97, 84];
 var total = 0;
